79c5ae8859bc8d326d161f79cb199c2694258b46
Qartde <bonifacemd2525@gmail.com>
2025-02-27 00:31:18 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 CRISS VEVO\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('*CRISS VEVO*', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 RAHMANI MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('*RAHMANI MD*', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
