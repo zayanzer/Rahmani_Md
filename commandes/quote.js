40f8bcd501707ff1a986a9e6a0b3b34808b26f5a
Qartde <bonifacemd2525@gmail.com>
2025-02-27 01:59:50 +0300

    Update quote.js

--- commandes/quote.js
+++ commandes/quote.js
@@ -13,14 +13,14 @@ zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) =
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║       CRISS VEVO              ║
+║       RAHMANI MD              ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by CRISS VEVO`);
+Powered by RAHMANI MD`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -33,14 +33,14 @@ Powered by CRISS VEVO`);
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║        CRISS VEVO              ║
+║        RAHMANI MD              ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by CRISS VEVO`);
+Powered by RAHMANI MD`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
